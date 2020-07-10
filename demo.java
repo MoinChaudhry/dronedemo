b89ad860134379bdc31c64279991806b68555d16
Moinch <43258060+Moinch@users.noreply.github.com>
2020-07-09 21:49:12 -0400

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,11 +1,10 @@
 public class demo {
     public static void main(String[] args) {
-        System.out.println("HELLO WORLD");
-        System.out.println("HELLO WORLD");
-        System.out.println("HELLO WORLD");
-        System.out.println("HELLO WORLD");
-        System.out.println("HELLO WORLD");
-        System.out.println("HELLO WORLD");
+        System.out.println("HELLO WORLD23sdag");
+        System.out.println("HELLO gadsgdWORLD");
+        System.out.println("HELLO WgaORLD");
+        System.out.println("HELLO WORLagagafgaD");
+        System.out.println("HELLO WgadgadORLD");
 
 
     }
