0022d90b1f95ce18cbade085a0e6b2c3738c2e14
Moinch <43258060+Moinch@users.noreply.github.com>
2020-07-09 21:36:38 -0400

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,5 +1,5 @@
+public class  {
     public static void main(String[] args) {
-		for (int i = 0; i < 10; i++) {
-			System.out.println("Hello World");
-		}
-	}
+        System.out.println("This will be printed");
+    }
+}
